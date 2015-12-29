22c551d2c499707defaa64031df056671abffa33
Weiming Zhai <wmzhai@gmail.com>
2015-12-29 15:28:38 +0800

    4.1 Add form for new tasks

--- App.jsx
+++ App.jsx
@@ -9,14 +9,6 @@ App = React.createClass({
         }
     },
 
-    getTasks() {
-        return [
-            { _id: 1, text: "This is task 1" },
-            { _id: 2, text: "This is task 2" },
-            { _id: 3, text: "This is task 3" }
-        ];
-    },
-
     renderTasks() {
         return this.data.tasks.map((task) => {
             return <Task key={task._id} task={task} />;
@@ -28,6 +20,9 @@ App = React.createClass({
             <div className="container">
                 <header>
                     <h1>Todo List</h1>
+                    <form className="new-task" onsubmit={this.handleSubmit}>
+                        <input type="text" ref="textInput" placeholder="Type to add new tasks"/>
+                    </form>
                 </header>
 
                 <ul>
