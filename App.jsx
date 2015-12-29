37564c0a0a9a480459f7d21dcb0eff7a22a374cd
Weiming Zhai <wmzhai@gmail.com>
2015-12-29 15:15:54 +0800

    3.2 Modify App components to get tasks from collection

--- App.jsx
+++ App.jsx
@@ -1,5 +1,14 @@
 // App component - represents the whole app
 App = React.createClass({
+
+    mixins: [ReactMeteorData],
+
+    getMeteorData(){
+        return {
+            tasks : Tasks.find({}).fetch()
+        }
+    },
+
     getTasks() {
         return [
             { _id: 1, text: "This is task 1" },
@@ -9,7 +18,7 @@ App = React.createClass({
     },
 
     renderTasks() {
-        return this.getTasks().map((task) => {
+        return this.data.tasks.map((task) => {
             return <Task key={task._id} task={task} />;
         });
     },
