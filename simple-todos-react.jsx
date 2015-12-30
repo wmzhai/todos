711aad63c4aaa2c56c4bad6f188bb6b9150797ea
Weiming Zhai <wmzhai@gmail.com>
2015-12-30 10:45:24 +0800

    Finish

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -40,10 +40,19 @@ Meteor.methods({
     },
 
     removeTask(taskId){
+        const task = Tasks.findOne(taskId);
+        if( task.private && task.owner !== Meteor.userId()){
+            throw  new Meteor.Error("not-authorized");
+        }
+
         Tasks.remove(taskId);
     },
 
     setChecked(taskId, setChecked){
+        const task = Tasks.findOne(taskId);
+        if( task.private && task.owner !== Meteor.userId()){
+            throw new Meteor.Error("not-authorized");
+        }
         Tasks.update(taskId, {$set: {checked:setChecked}});
     },
 
