76362d21c5cc302031c3be004362d3a1d87d1641
Weiming Zhai <wmzhai@gmail.com>
2016-01-03 21:34:46 +0800

    add Schema to Tasks

--- packages/todos-data/tasks.js
+++ packages/todos-data/tasks.js
@@ -1,10 +1,49 @@
 
 class TasksCollection extends Mongo.Collection{
-    insert(doc, callback) {
-        doc.createdAt = doc.createdAt || new Date();
-        const result = super(doc, callback);
-        return result;
-    }
+  insert(doc, callback) {
+    doc.createdAt = doc.createdAt || new Date();
+    const result = super(doc, callback);
+    return result;
+  }
 }
 
 Tasks = new TasksCollection("tasks");
+
+// Deny all client-side updates since we will be using methods to manage this collection
+Tasks.deny({
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; },
+});
+
+
+Tasks.schema = new SimpleSchema({
+  owner: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Id,
+    denyUpdate: true
+  },
+  text: {
+    type: String,
+    max: 100
+  },
+  username: {
+    type: String,
+    max: 100
+  },
+  createdAt: {
+    type: Date,
+    denyUpdate: true
+  },
+  checked: {
+    type: Boolean,
+    defaultValue: false
+  },
+  private: {
+    type: Boolean,
+    defaultValue: false
+  }
+});
+
+
+Tasks.attachSchema(Tasks.schema);
--- packages/todos-lib/package.js
+++ packages/todos-lib/package.js
@@ -1,44 +1,48 @@
 
 /* Information about this package */
 Package.describe({
-    // Short two-sentence summary
-    summary: 'common dependencies of all app packages',
+  // Short two-sentence summary
+  summary: 'common dependencies of all app packages',
 
-    // Version number
-    version: '0.0.1'
+  // Version number
+  version: '0.0.1'
 });
 
 /* This defines your actual package */
 Package.onUse(function(api){
-    //Language Stuff
-    api.imply([
-        'ecmascript@0.1.6',
-        'es5-shim@4.1.14'
-    ]);
+  //Language Stuff
+  api.imply([
+    'ecmascript@0.1.6',
+    'es5-shim@4.1.14'
+  ]);
 
-    // Collections
-    api.imply([
-        'mongo@1.1.3'
-    ]);
+  // Collections
+  api.imply([
+    'mongo',
+    'aldeed:collection2@2.5.0',
+    'dburles:collection-helpers@1.0.4',
+    'mdg:validation-error',
+    'reywood:publish-composite@1.4.2'
+  ]);
 
-    //Client-side libraries
-    api.imply([
-        'tracker',
-        'jquery',
-        'blaze-html-templates',
-        'session',
-        'react@0.14.3'
-    ]);
+  //Client-side libraries
+  api.imply([
+    'tracker',
+    'jquery',
+    'blaze-html-templates',
+    'session',
+    'react@0.14.3'
+  ]);
 
-    //Accounts
-    api.imply([
-        'accounts-ui@1.1.6',
-        'accounts-password@1.1.4'
-    ]);
+  //Accounts
+  api.imply([
+    'accounts-ui@1.1.6',
+    'accounts-password@1.1.4'
+  ]);
 
-    // DDP
-    api.imply([
-        'mdg:validated-method@0.2.0',
-        'aldeed:simple-schema@1.4.0',
-    ]);
+  // DDP
+  api.imply([
+    'mdg:validated-method@0.2.0',
+    'aldeed:simple-schema@1.4.0',
+  ]);
 });
