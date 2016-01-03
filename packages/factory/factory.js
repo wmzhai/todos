dbfe129b47af53869e6c386e918323eb7e0c37ae
Weiming Zhai <wmzhai@gmail.com>
2016-01-03 10:16:29 +0800

    init factory package info

--- packages/factory/factory.js
+++ packages/factory/factory.js
@@ -1,1 +1,10 @@
-// Write your package code here!
+
+/* global Factory:true */
+/* global LocalCollection */
+
+Factory = function(name, collection, properties) {
+  this.name = name;
+  this.collection = collection;
+  this.properties = properties;
+  this.afterBuildCbs = [];
+};
--- packages/factory/package.js
+++ packages/factory/package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'factory',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Factories for Meteor',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
@@ -12,13 +12,35 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('factory.js');
+
+  api.use([
+    'underscore',
+    'minimongo',
+    'random',
+    'ecmascript'
+  ]);
+
+  api.imply([
+    'dfischer:faker',
+    'random'
+  ]);
+
+  api.addFiles([
+    'factory.js',
+    'dataset.js',
+    'factory-api.js'
+  ]);
+
+  api.export('Factory');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('factory');
-  api.addFiles('factory-tests.js');
+  api.use([
+    'ecmascript',
+    'tinytest',
+    'factory',
+    'underscore'
+  ]);
+
+  api.addFiles('factory-tests.js','server');
 });
