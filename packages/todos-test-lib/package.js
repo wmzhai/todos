95e87863ade6adc807041df52ad65595d2588371
Weiming Zhai <wmzhai@gmail.com>
2016-01-03 15:18:03 +0800

    add todos-test-lib

---
+++ packages/todos-test-lib/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'todos-test-lib',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Common dependencies of all app tests',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.imply([
+    'todos-lib',
+    'practicalmeteor:mocha@2.1.0_5',
+    'practicalmeteor:chai@2.1.0_1',
+    'publication-collector',
+    'factory',
+    'stub-collections',
+    'ddp',
+  ]);
+});
