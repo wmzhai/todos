3cb90c4b7fee18cc61a6bd6112abeb08f42e2030
Weiming Zhai <wmzhai@gmail.com>
2016-01-03 15:07:10 +0800

    finish stub-collections

--- packages/stub-collections/package.js
+++ packages/stub-collections/package.js
@@ -2,23 +2,32 @@ Package.describe({
   name: 'stub-collections',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
+  summary: 'Stub out all collections with temporary local collections',
+
+  debugOnly:true,
+
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
+
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use('ecmascript');
+  api.use([
+    'ecmascript',
+    'mongo',
+    'practicalmeteor:sinon@1.14.1_2',
+    'underscore'
+  ]);
   api.addFiles('stub-collections.js');
+  api.export('StubCollections');
 });
 
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
   api.use('stub-collections');
+  api.use('mongo');
   api.addFiles('stub-collections-tests.js');
 });
