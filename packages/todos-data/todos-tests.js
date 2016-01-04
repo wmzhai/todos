a7652dffdccdee3d7673068ac1bd1d2f163300f5
Weiming Zhai <wmzhai@gmail.com>
2016-01-04 12:00:59 +0800

    add first chai test skeleton

---
+++ packages/todos-data/todos-tests.js
@@ -0,0 +1,10 @@
+
+const assert = chai.assert;
+
+describe('tasks', () => {
+  describe('mutators', () => {
+    it('builds correctly from factory', () =>{
+      assert.equal(true,true);
+    })
+  });
+});
