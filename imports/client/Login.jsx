c66efabf26c5f298b472f6bc23d798b34079a43d
Weiming Zhai <wmzhai@gmail.com>
2016-01-11 17:01:45 +0800

    add login route and component

---
+++ imports/client/Login.jsx
@@ -0,0 +1,11 @@
+
+Login = React.createClass({
+
+  render(){
+    return(
+      <div className="container">
+        <h1>Login</h1>
+      </div>
+    )
+  }
+});
--- main/client/routes.jsx
+++ main/client/routes.jsx
@@ -1,7 +1,7 @@
 import { FlowRouter } from 'kadira:flow-router';
 import { ReactLayout } from 'kadira:react-layout';
 import { App } from '../../imports/client/App.jsx';
-import { MainLayout } from '../../imports/client/layouts/MainLayout.jsx';
+import { MainLayout } from '../../imports/client/MainLayout.jsx';
 
 Meteor.subscribe("tasks");
 
@@ -9,5 +9,12 @@ FlowRouter.route( '/', {
   name: 'home',
   action() {
     ReactLayout.render( MainLayout, {content: <App />} );
+  }
+});
+
+FlowRouter.route( '/login', {
+  name: 'login',
+  action() {
+    ReactLayout.render( MainLayout, {content: <Login />});
   }
 });
