5c3c3ae406378790c1fe87362a14ec64b36c2680
Weiming Zhai <wmzhai@gmail.com>
2016-01-12 15:04:30 +0800

    MainLayout using new style

--- imports/client/MainLayout.jsx
+++ imports/client/MainLayout.jsx
@@ -3,8 +3,24 @@
 export const MainLayout = React.createClass({
   render() {
     return (
-      <div>
-        {this.props.content}
+      <div id="container" className="menu-open">
+        <section id="menu">
+          <div className="btns-group">
+            <a href="#" className="btn-secondary">Sign In</a>
+            <a href="#" className="btn-secondary">Join</a>
+          </div>
+
+          <div className="list-todos">
+            <a className="js-new-list link-list-new">
+              <span className="icon-plus"></span>
+              New List
+            </a>
+          </div>
+        </section>
+
+        <div id="content-container">
+          {this.props.content}
+        </div>
       </div>
     );
   }
